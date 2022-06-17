c109a66221811d1f18880d8c86a1a6bcba9cef38
Jade <jade_harding@hotmail.com>
2022-06-17 15:49:02 +1000

    commit

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ export const Heading3 = styled.h1`
   text-align: center;
   padding-bottom: 15%;
   -webkit-text-stroke: 1px black;
-  @media (min-width: 1080px) {
+  @media (max-width: 1080px) {
     font-size: 25pt;
   }
 `;
