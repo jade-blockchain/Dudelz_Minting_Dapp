cea3222cfc85532431be25071dc7aae26d4e62c2
Jade <jade_harding@hotmail.com>
2022-06-18 16:53:13 +1000

    commit

--- src/App.js
+++ src/App.js
@@ -1101,9 +1101,7 @@ function App() {
 	</div>
 	  </header>
 	  <section>
-		  <div class="container">
-			  <img src={logo} />
-		  </div>
+		  <div class="container" src={logo} />
           <div class="containermain" style={{
               backgroundColor: "rgb(255, 255, 255)",
               padding: 40,
