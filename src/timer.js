069211d8c20d2b2ac6cae3238464d97d6c125fd2
nemanja2905 <neksonnekson2905@gmail.com>
2022-08-31 04:16:36 +0300

    update centered, add Timer

---
+++ src/timer.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+
+export default function Timer() {
+  const [timeState, setTimeState] = useState(60 * 60 * 24);
+
+  useEffect(() => {
+    const timer = setTimeout(function () {
+      setTimeState(timeState - 1);
+      if (timeState < 1) {
+        setTimeState(60 * 60 * 24);
+      }
+    }, 1000);
+
+    return () => {
+      // this should work flawlessly besides some milliseconds lost here and there
+      clearTimeout(timer);
+    };
+  }, [timeState]);
+  return (
+    <div className="timer">
+      <div className="box-h">
+        <span className="box-num">{secondsToTime(timeState).h}</span>
+        <span className="box-title-h">H O U R S</span>
+      </div>
+      <div className="box-m">
+        <span className="box-num">{secondsToTime(timeState).m}</span>
+        <span className="box-title-m">M I N U T E S</span>
+      </div>
+      <div className="box-s">
+        <span className="box-num">{secondsToTime(timeState).s}</span>
+        <span className="box-title-s">S E C O N D S</span>
+      </div>
+    </div>
+  );
+}
+
+function secondsToTime(secs) {
+  let hours = Math.floor(secs / (60 * 60));
+
+  let divisor_for_minutes = secs % (60 * 60);
+  let minutes = Math.floor(divisor_for_minutes / 60);
+
+  let divisor_for_seconds = divisor_for_minutes % 60;
+  let seconds = Math.ceil(divisor_for_seconds);
+
+  let obj = {
+    h: hours,
+    m: minutes,
+    s: seconds,
+  };
+  return obj;
+}
