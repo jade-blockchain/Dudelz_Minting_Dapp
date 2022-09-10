ef021756fb65a08fdb1714c9149cbe44147b42bf
Jade <jade_harding@hotmail.com>
2022-09-10 15:32:25 +1000

    commit

--- src/App.js
+++ src/App.js
@@ -881,6 +881,10 @@ function App() {
 
   async function connectwallet() {
     if (window.ethereum) {
+      var web3 = new Web3(window.ethereum);
+      await window.ethereum.send('eth_requestAccounts');
+      var accounts = await web3.eth.getAccounts();
+      account = accounts[0];
       contract = new web3.eth.Contract(ABI, ADDRESS);
       dispatch(connect());
     }
